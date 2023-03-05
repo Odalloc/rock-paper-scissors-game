74a8dc834be0c2872431b19ef2a4e6dd4c439b33
Odalloc <70327008+Odalloc@users.noreply.github.com>
2023-03-05 21:20:51 +0800

    Add game round result function

--- script.js
+++ script.js
@@ -1,43 +1,65 @@
-console.log("hello world");
+let computerScore = 0;
+let playerScore = 0;
 
 function getComputerChoice() {
   let gameChoice = ["rock", "paper", "scissors"];
+
   let gameChoiceResult =
     gameChoice[Math.floor(Math.random() * gameChoice.length)];
+
   return gameChoiceResult;
 }
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "paper") {
+    computerScore++;
     return "You Lose! Paper beats Rock";
   }
 
   if (playerSelection == "paper" && computerSelection == "scissors") {
+    computerScore++;
     return "You Lose! Scissors beats Paper";
   }
 
   if (playerSelection == "scissors" && computerSelection == "rock") {
+    computerScore++;
     return "You Lose! Rock beats Scissors ";
   }
 
   if (computerSelection == "rock" && playerSelection == "paper") {
+    playerScore++;
     return "You Win! Paper beats Rock";
   }
 
   if (computerSelection == "paper" && playerSelection == "scissors") {
+    playerScore++;
     return "You Win! Scissors beats Paper";
   }
 
   if (computerSelection == "scissors" && playerSelection == "rock") {
-    return "You Win! Rock beats Scissors ";
+    playerScore++;
+    return "You Win! Rock beats Scissors";
   }
 
   return "It's a Draw";
 }
 
+function game() {
+  for (let x = 0; x < 5; x++) {
+    playRound(playerSelection, getComputerChoice());
+  }
+
+  return getRoundResult();
+}
+
+function getRoundResult() {
+  if (playerScore > computerScore) {
+    return "You win this round";
+  } else {
+    return "You lose this round";
+  }
+}
+
 const playerSelection = "rock";
-const computerSelection = getComputerChoice();
 
-console.log(computerSelection);
-
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
